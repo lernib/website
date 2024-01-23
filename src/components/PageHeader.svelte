26f2f7897e83ef0a55e1048cc42f24ac2570b089
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-01-23 12:49:34 -0500

    Added header

---
+++ src/components/PageHeader.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  const LINKS = [
+    ["/contact", "Contact"],
+    ["/pricing", "Pricing"],
+    ["/about", "About"]
+  ]
+</script>
+
+<div class="header">
+  <img class="header-img"
+    src="/logo128.png"
+    alt="logo"
+  />
+  <nav>
+    {#each LINKS as [url, text]}
+      <a href={url}>{text}</a>
+    {/each}
+  </nav>
+</div>
+
+<style>
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: calc(100vw - 3rem);
+  padding: 1.5rem;
+  font-size: 0.75rem;
+  background-color: var(--color1);
+}
+
+.header-img {
+  height: 1.25rem;
+}
+
+.header > nav {
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: end;
+  align-items: center;
+  color: blue;
+  column-gap: 2rem;
+}
+</style>
