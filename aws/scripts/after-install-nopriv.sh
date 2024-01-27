c03289704dca0b4d3d88f29a806e237b995e45ff
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-01-27 17:13:59 -0500

    Added CodeDeploy

---
+++ aws/scripts/after-install-nopriv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir ~/app
+aws s3 cp s3://lernib-bucket.build/static/ ~/app --recursive
+aws s3 cp s3://lernib-bucket.build/artifacts/ ~/app --recursive
+
+##############################
+# INSTALL NODE
+##############################
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 20
