387768d723c713ecf8d9a5f555f3db28be4347a6
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-01-27 17:13:59 -0500

    Added CodeDeploy

---
+++ aws/scripts/after-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -xe
+
+##############################
+# INSTALL HTTPD
+##############################
+yum update -y
+yum install httpd -y
+
+##############################
+# CONFIGURE HTTPD
+##############################
+rm -f /etc/httpd/conf/httpd.conf
+cp /usr/local/codedeployresources/httpd.conf /etc/httpd/conf/httpd.conf
+cp /usr/local/codedeployresources/site.conf /etc/httpd/conf.d/site.conf
+cp /usr/local/codedeployresources/site-ssl.conf /etc/httpd/conf.d/site-ssl.conf.disabled
+
+yum install certbot python3-certbot-apache -y
+certbot certonly --apache --non-interactive --agree-tos --email support@lernib.com -d lernib.com -d www.lernib.com
+
+mv /etc/httpd/conf.d/site-ssl.conf.disabled /etc/httpd/conf.d/site-ssl.conf
+systemctl restart httpd
+
+mv /usr/local/codedeployresources/site.service /etc/systemd/system/site.service
+systemctl enable site
+systemctl start site
