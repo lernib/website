18903895bac133b9d308fc18b986c86fed4a25fa
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-02-01 13:20:59 -0500

    Added student pages

---
+++ src/lib/types.ts
@@ -0,0 +1,7 @@
+
+export interface ApiStudent {
+  userid: string,
+  student_name: string,
+  client_name: string,
+  timezone: "-5" | "-6" | "-7" | "-8" | "NA"
+}
---
+++ src/routes/(locked)/dashboard/students/[userid]/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import type { PageServerData } from "./$types";
+  import { TIMEZONES } from "$lib/config";
+
+  export let data: PageServerData;
+</script>
+
+<main>
+  <h1>
+    {data.student.student_name}
+  </h1>
+  <hr />
+
+  <table
+    cellspacing=0
+  >
+    <tr>
+      <td>
+        Client Name
+      </td>
+      <td>
+        {data.student.client_name}
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Student Name
+      </td>
+      <td>
+        {data.student.student_name}
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Timezone
+      </td>
+      <td>
+        {TIMEZONES[data.student.timezone]}
+      </td>
+    </tr>
+  </table>
+</main>
+
+<style lang="scss">
+  main {
+    align-self: stretch;
+    flex-grow: 1;
+    padding: 2rem;
+  }
+
+  h1 {
+    font-size: 2.5rem;
+    font-weight: bold;
+  }
+
+  hr {
+    margin-bottom: 2rem;
+  }
+
+  td:not(:last-child) {
+    padding-right: 4rem;
+  }
+</style>
