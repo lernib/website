f7568e9affe7c155e3091fd9f463a433afcd345c
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-02-05 11:42:15 -0500

    Fixed API route security

--- src/routes/(locked)/dashboard/students/+server.ts
+++ src/routes/(locked)/dashboard/students/+server.ts
@@ -1,12 +1,12 @@
 import type { RequestHandler } from "./$types";
-import { verifyTokens } from "$lib/auth/helpers";
+import { accessTokenPayload } from "$lib/auth/helpers";
 import { error } from "@sveltejs/kit";
 
 
 export const POST: RequestHandler = async ({ request, cookies }) => {
-  const access_token = cookies.get('access_token');
-  
-  if (access_token && await verifyTokens(access_token)) {
+  const access_token = await accessTokenPayload(cookies.get('access_token'));
+
+  if (access_token?.["cognito:groups"]?.includes('maintenance')) {
     const response = await fetch('https://api.lernib.com/dev/students', {
       method: 'POST',
       headers: {
