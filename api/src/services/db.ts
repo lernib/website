52729aa40ff873b8c74ba31e02c66932c4788ace
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-02-09 13:28:15 -0500

    Added DynamoDB region

--- api/src/services/db.ts
+++ api/src/services/db.ts
@@ -1,7 +1,9 @@
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 
-const dynamo = DynamoDBDocument.from(new DynamoDB());
+const dynamo = DynamoDBDocument.from(new DynamoDB({
+	region: 'us-east-1'
+}));
 const TABLE_NAME = 'LernibData-Students';
 
 export { dynamo, TABLE_NAME };
