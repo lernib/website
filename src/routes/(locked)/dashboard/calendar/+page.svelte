75d67922fdcb1a94a0b86774edceae937b3d7a4f
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-02-17 12:55:50 -0500

    Added FullCalendar

--- src/routes/(locked)/dashboard/calendar/+page.svelte
+++ src/routes/(locked)/dashboard/calendar/+page.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
-  import Calendar from "$components/widgets/Calendar.svelte";
+  import { onMount } from 'svelte';
+  import { Calendar } from '@fullcalendar/core';
+  import dayGridPlugin from '@fullcalendar/daygrid';
+  import timeGridPlugin from '@fullcalendar/timegrid';
+  import listPlugin from '@fullcalendar/list';
+
+  let calendarEl: HTMLDivElement;
+
+  onMount(async () => {
+    let calendar = new Calendar(calendarEl, {
+      plugins: [
+        dayGridPlugin,
+        timeGridPlugin,
+        listPlugin
+      ],
+      initialView: 'timeGridWeek',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,listWeek'
+      }
+    });
+
+    calendar.render();
+  });
 </script>
 
 <svelte:head>
@@ -7,10 +31,15 @@
 </svelte:head>
 
 <main>
-  <Calendar />
+  <div class="calendar" bind:this={calendarEl} />
 </main>
 
 <style lang="scss">
+  :global(.sk-core) {
+    width: 100vw;
+    height: 100vh;
+  }
+
   main {
     align-self: stretch;
     flex-grow: 1;
@@ -21,5 +50,10 @@
     justify-content: start;
     align-items: start;
     column-gap: 1rem;
+
+    > .calendar {
+      align-self: stretch;
+      flex-grow: 1;
+    }
   }
 </style>
