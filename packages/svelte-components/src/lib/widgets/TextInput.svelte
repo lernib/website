3bb8962d2f9caeee056fff3d28fa80312a73db73
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-02-21 15:03:44 -0500

    Added storybook

--- packages/svelte-components/src/lib/widgets/TextInput.svelte
+++ packages/svelte-components/src/lib/widgets/TextInput.svelte
@@ -1,4 +1,14 @@
 <script lang="ts">
 </script>
 
-<input type="text" />
+<input type="text" class="widgetR" />
+
+<style lang="scss">
+  .widgetR {
+    outline: none;
+    padding: 0.5em;
+    font-size: 1.25rem;
+    border-radius: 0.5em;
+    border: 2px solid black;
+  }
+</style>
