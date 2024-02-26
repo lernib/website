afa94610bee242c38f9dd133d24b9b18cc67e4f3
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-02-26 17:51:23 -0500

    Added calendar API to Adonis

--- adonis/start/routes.ts
+++ adonis/start/routes.ts
@@ -9,10 +9,16 @@
 
 import router from '@adonisjs/core/services/router'
 import StudentsController from '#controllers/students_controller'
+import EventsController from '#controllers/events_controller'
 
 router.get('/students', [StudentsController, 'index'])
-
 router.get('/student/:id', [StudentsController, 'show'])
 router.delete('/student/:id', [StudentsController, 'destroy'])
 router.patch('/student/:id', [StudentsController, 'update'])
 router.post('/student', [StudentsController, 'store'])
+
+router.get('/events', [EventsController, 'index'])
+router.get('/event/:id', [EventsController, 'show'])
+router.delete('/event/:id', [EventsController, 'destroy'])
+router.patch('/event/:id', [EventsController, 'update'])
+router.post('/event', [EventsController, 'store'])
