d4c088407e7d86eac67279a17b7b30e7089bc20f
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-02-27 19:02:50 -0500

    Added new codedeploy appspec for dev

--- aws/codedeploy/prepare_dev.py
+++ aws/codedeploy/prepare_dev.py
@@ -9,7 +9,7 @@ EC2_DIR = path.join(PYDIR, '../ec2')
 
 with ZipFile(path.join(PYDIR, 'archive_dev.zip'), 'w') as archive:
     # Write appspec.yml
-    archive.write(path.join(PYDIR, '../../appspec.yml'), 'appspec.yml')
+    archive.write(path.join(PYDIR, '../../appspec.dev.yml'), 'appspec.yml')
     # Write all of the contents of aws/ec2
     for root, dirs, files in os.walk(EC2_DIR):
         root_rel = path.relpath(root, EC2_DIR)
---
+++ aws/ec2/dev/after-install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -xe
+
+export RESPATH=/usr/local/codedeployresources
+
+##############################
+# INSTALL HTTPD
+##############################
+yum update -y
+yum install httpd -y
+
+##############################
+# CONFIGURE HTTPD
+##############################
+rm -f /etc/httpd/conf/httpd.conf
+cp $RESPATH/httpd.conf /etc/httpd/conf/httpd.conf
+cp $RESPATH/dev/site.conf /etc/httpd/conf.d/site.conf
+cp $RESPATH/dev/site-ssl.conf /etc/httpd/conf.d/site-ssl.conf.disabled
+cp $RESPATH/dev/api.conf /etc/httpd/conf.d/api.conf
+cp $RESPATH/dev/api-ssl.conf /etc/httpd/conf.d/api-ssl.conf.disabled
+
+
+yum install certbot python3-certbot-apache -y
+certbot certonly --apache --non-interactive --agree-tos --email support@lernib.com -d dev.lernib.com -d www.lernib.com
+certbot certonly --apache --non-interactive --agree-tos --email support@lernib.com -d api.dev.lernib.com
+
+mv /etc/httpd/conf.d/site-ssl.conf.disabled /etc/httpd/conf.d/site-ssl.conf
+mv /etc/httpd/conf.d/api-ssl.conf.disabled /etc/httpd/conf.d/api-ssl.conf
+systemctl restart httpd
+
+mv $RESPATH/site.service /etc/systemd/system/site.service
+mv $RESPATH/api.service /etc/systemd/system/api.service
+
+systemctl enable site
+systemctl restart site
+systemctl start site
+
+systemctl enable api
+systemctl restart api
+systemctl start api
+
+systemctl enable httpd
+systemctl start httpd
