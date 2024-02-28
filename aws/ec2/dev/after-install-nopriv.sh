53869d82292c198b5408366c91e72c824d6943f5
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-02-28 15:01:08 -0500

    Added dev server migrations

---
+++ aws/ec2/dev/after-install-nopriv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir ~/app
+mkdir ~/api
+
+aws s3 cp s3://lernib-bucket.build/ec2/sveltekit/static/herobg.jpg ~/app
+aws s3 cp s3://lernib-bucket.build/ec2/sveltekit/artifacts/ ~/app --recursive
+
+aws s3 cp s3://lernib-bucket.build/ec2/api/static/ ~/api --recursive
+aws s3 cp s3://lernib-bucket.build/ec2/api/artifacts/build ~/api --recursive
+
+##############################
+# INSTALL NODE
+##############################
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 20
+
+cd ~/app
+npm i --production
+
+cd ~/api
+npm i --production
+node ace migration:run
