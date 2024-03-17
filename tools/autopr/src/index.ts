e6f07d06f543f731c2b73acbb4294059f3f447a1
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-03-17 17:32:16 -0400

    Added environment accessing

--- tools/autopr/src/index.ts
+++ tools/autopr/src/index.ts
@@ -9,6 +9,28 @@ const parsed = cli.parse();
 
 const octokit = new Octokit({
   auth: parsed.options.github
-})
+});
 
+function env_required(key: string): string {
+  const res = process.env[key];
 
+  if (!res) {
+    throw new Error(`Environment variable ${key} not set.`);
+  }
+
+  return res;
+}
+
+const [REPO_OWNER, REPO_NAME] = env_required('GITHUB_REPOSITORY').split('/');
+const BRANCH = env_required('GITHUB_REF_NAME');
+const ROOT = env_required('GITHUB_WORKSPACE');
+
+async function get_all_branches() {
+  await octokit.request('GET /repos/{owner}/{repo}/branches', {
+    owner: REPO_OWNER,
+    repo: REPO_NAME,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+}
