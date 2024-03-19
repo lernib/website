920784f3a273a88cb61dad7579edf57cac9fd85d
Carlos Kieliszewski <ckieliszewski@lernib.com>
2024-03-19 15:53:40 -0400

    Added on_push full logic

--- tools/autopr/src/config.ts
+++ tools/autopr/src/config.ts
@@ -6,12 +6,45 @@ interface Config {
   affects: Record<string, string>
 }
 
+let config: Config | undefined = undefined;
+
 async function config_path(): Promise<string> {
   return path.join((await globalCtx()).root, '.github/autopr.json');
 }
 
-export async function config(): Promise<Config> {
-  const contents = await fs.readFile(await config_path(), 'utf-8');
+async function getConfig(): Promise<Config> {
+  if (config) return config;
 
-  return JSON.parse(contents);
+  const contents = await fs.readFile(await config_path(), 'utf-8');
+  config = JSON.parse(contents);
+
+  return getConfig();
 }
+
+type AffectsBranchFilter = (name: string) => boolean;
+function affectsBranchFilter(affects: string[]): AffectsBranchFilter {
+  const regexes = affects.map(s => new RegExp(s));
+
+  const predicate = (name: string) => {
+    for (let r of regexes) {
+      if (r.test(name)) return true;
+    }
+
+    return false;
+  }
+
+  return predicate;
+}
+
+function getAffects(config: Config, branches: string[], cause: string): string[] {
+  const affects = Object.entries(config.affects)
+    .filter(([c, _]) => c == cause)
+    .map(([_, eff]) => eff);
+  
+  const filter = affectsBranchFilter(affects);
+  
+  return branches
+    .filter(filter);
+}
+
+export { getConfig as config, getAffects };
